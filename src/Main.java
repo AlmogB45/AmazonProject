119985817790c7a224cd2edd38d5f090c880427f
Almog Ben Yamin <almoggx@gmail.com>
2023-07-12 15:22:47 +0300

    Added Login / Register function Added FileHandler class Added LoginManager class Added User Class Changed Book class.

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static java.lang.System.exit;
-import static java.lang.System.setOut;
 
 public class Main {
     public static void main(String[] args) {
